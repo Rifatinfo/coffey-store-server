801ca05704072f095fc3ac8ef6385b9806916ae8
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-09-19 18:18:31 +0600

    delete function work

--- index.js
+++ index.js
@@ -58,9 +58,9 @@ async function run() {
       res.send(result);
     });
     
-    app.delete("/coffey/:id", async(req, re) => {
+    app.delete("/coffey/:id", async(req, res) => {
       const id = req.params.id;
-      const query = {_id:  ObjectId(_id)}
+      const query = {_id: new ObjectId(id)}
       const result = await coffeyCollection.deleteOne(query);
       res.send(result);
     });
